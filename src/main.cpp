628a087ddc01f046f3e26e6002bb61c14eb536e5
Jirubizu <zilvysdovydas@yahoo.co.jp>
2019-03-20 20:25:11 +0000

    Getting user to input their own file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  std::string PathOfFile;
+  std::cout << "Please enter an input" << std::endl;
+  std::getline(std::cin, PathOfFile);
+  std::ifstream file(PathOfFile); // create an object of the file
+  if (!file.is_open()) {
+    std::cout << "File not found\n";
+  } else {
+    std::cout << "File Found";
+  }
   return 0;
 }
